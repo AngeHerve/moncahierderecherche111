8849be09c412a53e3497ed3ce6b3876c969b84a0
Nguessan03 <108245015+Nguessan03@users.noreply.github.com>
2022-09-04 00:52:53 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -1,22 +1,87 @@
-﻿<?php
-	// Initialiser la session
-	session_start();
-	// Vérifiez si l'utilisateur est connecté, sinon redirigez-le vers la page de connexion
-	if(!isset($_SESSION["username"])){
-		header("Location: login.php");
-		exit();
-	}
-?>
 <!DOCTYPE html>
-<html>
-	<head>
-	<link rel="stylesheet" href="style.css" />
-	</head>
-	<body>
-		<div class="sucess">
-		<h1>Bienvenue <?php echo $_SESSION['username']; ?>!</h1>
-		<p>C'est votre espace utilisateur.</p>
-		<a href="logout.php">Déconnexion</a>
-		</div>
-	</body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sing-in</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0/css/bootstrap.min.css" integrity="sha512-XWTTruHZEYJsxV3W/lSXG1n3Q39YIWOstqvmFsdNEEQfHoZ6vm6E9GK2OrF6DSJSpIbRbi+Nn0WDPID9O7xB2Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+   <main class="bg-sign-in d-flex justify-content-center align-items-center">
+      <div class=" form-sign-in bg-white mt-2 h-auto mb-2 text-center pt-2 pe-4 ps-4 d-flex flex-column">
+        <h1 class="E-classe text-start ms-3 ps-1" >E-classe</h1>
+        <div>
+          <h2 class=" sign-in text-uppercase">sign in</h2>
+        <p>Enter your credentials to access your account</p>
+        </div>
+        <?php
+          if(isset($_GET['error'])){
+            if($_GET['error'] == "please enter your email or password"){
+              echo '<div sclass="alert alert-danger" role="alert">
+            please enter your email or password
+          </div>';
+            }
+            elseif($_GET['error'] == "email or password not found"){
+              echo '<div class="alert alert-danger" role="alert">
+              email or password not found
+          </div>';
+            }
+          }    
+        ?>
+        <form method="POST" action="login.php" enctype="multipart/form-data">
+          <div class="mb-3 mt-3 text-start">
+            <label for="email">Email:</label>
+            <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" value="<?php  if(isset($_COOKIE['email'])){echo $_COOKIE['email']; }?>">
+          </div>
+          <div class="mb-3 text-start">
+            <label for="pwd">Password:</label>
+            <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pass" value="<?php  if(isset($_COOKIE['password'])){echo $_COOKIE['password']; }?>">
+          </div>
+          <div class="mb-3 form-check d-flex gap-2">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1" name="check">
+    <label class="form-check-label" for="exampleCheck1">Remember Me</label>
+  </div>
+          <button type="submit" name="submit" class="btn text-white w-100 text-uppercase">sign in</button>
+          <p class="mt-4">Forgot your password? <a href="resetpass.php">Reset Password</a></p>
+          
+        </form>
+        <button href="" class="btn-register btn btn-success mb-3">Create Account</button>
+     </div>
+
+   </main>
+
+          <div class="bg"></div>
+
+   <div class="register d-flex justify-content-center align-items-center">
+   <div class="sign-up bg-white mt-2 h-auto mb-2 text-center pt-4 pb-3 pe-4 ps-4 d-flex flex-column">
+       <div>
+           <h2 class=" sign-in text-uppercase">sign up</h2>
+       </div>
+       <form method="POST" id="signup" action="createaccout.php">
+           <div class="mb-3 mt-3 text-start">
+               <label class="label-signup" for="username">username: <span class="valid"></span></label>
+               <input type="text" class="form-control" id="username" placeholder="Enter username" name="username">
+           </div>
+           <div class="mb-3 text-start">
+               <label class="label-signup" for="email">Email: <span class="valid"></span></label>
+               <input type="email" class="form-control" id="Email" placeholder="Enter Email" name="email">
+           </div>
+           <div class="mb-3 text-start">
+               <label class="label-signup" for="pwd">Create Password: <span class="valid"></span></label>
+               <input type="password" class="form-control" id="Pwd" placeholder="Enter password" name="pass" autocomplete="on">
+           </div>
+           <div class="mb-3 text-start">
+               <label class="label-signup" for="conPwd">Confirm Password: <span class="valid"></span></label>
+               <input type="password" class="form-control" id="conPwd" placeholder="Confirm password" name="conPass" autocomplete="on">
+           </div>
+           <button type="submit" name="submit" class="btn text-white w-100 text-uppercase">sign up</button>
+           <p class="mt-4">you have account? <a href="index.php">sign-in</a></p>
+       </form>
+   </div>
+   </div>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/js/bootstrap.bundle.min.js" integrity="sha512-9GacT4119eY3AcosfWtHMsT5JyZudrexyEVzTBWV3viP/YfB9e2pEy3N7WXL3SV6ASXpTU0vzzSxsbfsuUH4sQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+   <script src="./js/validation.js"></script>
+</body>
 </html>
--- login.php
+++ login.php
@@ -1,68 +1,29 @@
 <?php
-session_start();
-include('config.php');
+        session_start();
+        if(isset($_POST['submit'])){
+          include './pages/conixion.php';
+          $email = $_POST['email'];
+          $password = $_POST['pass'];
 
-$username = "";
-$email    = "";
-$errors = array();
-
-if (isset($_POST['submit'])) {
-  $username = mysqli_real_escape_string($conn, $_POST['username']);
-  $password_1 = mysqli_real_escape_string($conn, $_POST['password']);
-
-  if (empty($username)) {
-    array_push($errors, "Username is required");
-  }
-  if (empty($password_1)) {
-    array_push($errors, "password is required");
-  }
-
-  if (count($errors) == 0) {
-    $password = ($password_1);
-    $query = "SELECT * FROM users WHERE Name_users='$username' AND Pwd_users='$password'";
-    $results = mysqli_query($conn, $query);
-    if (mysqli_num_rows($results) == 1) {
-      $_SESSION['username'] = $username;
-      $_SESSION['success'] = "You are now logged in";
-      header('location: index.php');
-    }else {
-			$message = "Le nom d'utilisateur ou le mot de passe est incorrect.";
-    }
-  }
-}
-
-?>
-﻿<!DOCTYPE html>
-<html>
-<head>
-	<link rel="stylesheet" href="modi.css" />
-  <style media="screen">
-  .box1{
-    border: 1px solid #c4c4c4;
-    border-radius: 40px;
-    margin-left: 30px
-  }
-  img{
-    margin-right: 10px;
-    position: absolute;
-    margin-left: 800px;
-  }
-  </style>
-</head>
-<body>
-  <div class="">
-    <img src="2.jpeg" alt="">
-  </div>
-<form class="box1" action="" method="post" name="login">
-	 <?php include('error.php') ?>
-<h1 class="box-title">Se Connecter</h1>
-<input type="text" class="box-input" name="username" placeholder="Nom d'utilisateur">
-<input type="password" class="box-input" name="password" placeholder="password">
-<button type="submit" value="Connexion " name="submit" class="box-button">Valider</button>
-<p class="box-register">Vous êtes nouveau ici? <a href="conntion.php">Crée un compte</a></p>
-<?php if (! empty($message)) { ?>
-    <p class="errorMessage"><?php echo $message; ?></p>
-<?php } ?>
-</form>
-</body>
-</html>
+          $requete = "SELECT * FROM users WHERE Email = '$email' and Password = '$password'";
+          $statment = $con -> prepare($requete);
+          $statment -> execute();
+          $result = $statment -> fetch();
+          if($result['Email'] === $email && $result['Password'] === $password){
+            $_SESSION['name'] = $result['username'];
+            $_SESSION['email'] = $result['Email'];
+            $_SESSION['password'] = $result['Password'];
+            if(isset($_POST['check'])){
+                setcookie('email',$_SESSION['email'],time() + 3600);
+                setcookie('password',$_SESSION['password'],time() + 3600);
+            }
+            header("location:./pages/dashboard.php");
+            }
+            else if(empty($email) || empty($password)){
+                header("location:index.php?error=please enter your email or password");
+            }
+            else
+            {
+                header("location:index.php?error=email or password not found");
+            }
+      }?>
